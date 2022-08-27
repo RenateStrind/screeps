2e07cd50374eefba9c7962c5570258ee2fe9e5ed
RenateStrind <RenateStrind@gmail.com>
2022-08-27 13:04:28 -0500

    added example

--- testing/Shorts.js
+++ testing/Shorts.js
@@ -1,4 +1,5 @@
 Game.spawns['Spawn1'].spawnCreep( [WORK, CARRY, MOVE], 'Builder1',
     { memory: { role: 'builder' } } );
 
-    
+//Manually add memory to creep
+Memory.creeps['Harvester50760810'] = {role: 'harvester'};
